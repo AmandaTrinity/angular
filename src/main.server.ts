64221f776b8adf6a740929c4f25020a30af0faa2
Amanda Trinity <amandatrinity16@gmail.com>
2025-12-23 14:09:10 -0300

    feat: angular structure

---
+++ src/main.server.ts
@@ -0,0 +1,9 @@
+//boostrap(ponta pé) do server que vai renderizar minha aplicação e retornar html estático para o cliente SSR
+import { BootstrapContext, bootstrapApplication } from '@angular/platform-browser';
+import { App } from './app/app';
+import { config } from './app/app.config.server';
+
+const bootstrap = (context: BootstrapContext) =>
+    bootstrapApplication(App, config, context);
+
+export default bootstrap;
